51baf1581c6e28bcca2821ab4fd03cf5130ee1ea
Глеб Безруков <g.bezrukov@reo.ru>
2022-11-03 12:01:51 +0300

    Partial implementation of the table and card

--- TopPokerBot.Domain/Card.cs
+++ TopPokerBot.Domain/Card.cs
@@ -3,4 +3,6 @@
 /// <summary>
 /// Record describing the card model
 /// </summary>
-public record Card;
+/// <param name="Suit"> The suit of the card </param>
+/// <param name="KindOfRank"> The kind of rank of the card </param>
+public record Card(Suit Suit, KindOfRank KindOfRank);
--- TopPokerBot.Domain/Table.cs
+++ TopPokerBot.Domain/Table.cs
@@ -5,5 +5,46 @@
 /// </summary>
 public class Table : IAggregateRoot
 {
+	private readonly HashSet<Card> _cardDeck = new();
 
+	/// <summary>
+	/// .ctr
+	/// </summary>
+	public Table(Guid id, int number)
+	{
+		Id = id;
+
+		Number = number;
+
+		var suits = Enum.GetValues(typeof(Suit))
+			.Cast<Suit>()
+			.ToArray();
+
+		var kindsOfRanks = Enum.GetValues(typeof(KindOfRank))
+			.Cast<KindOfRank>()
+			.ToArray();
+
+		foreach (var suit in suits)
+		{
+			foreach (var kindOfRank in kindsOfRanks)
+			{
+				_cardDeck.Add(new(suit, kindOfRank));
+			}
+		}
+	}
+
+	/// <summary>
+	/// Identifier
+	/// </summary>
+	public Guid Id { get; }
+
+	/// <summary>
+	/// Number of table
+	/// </summary>
+	public int Number { get; }
+
+	/// <summary>
+	/// Card deck
+	/// </summary>
+	public IReadOnlyCollection<Card> CardDeck => _cardDeck;
 }
